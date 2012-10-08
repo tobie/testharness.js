fd9730c3d4130155e98716a1a3cd50f08983773b
Tobie Langel <tobie.langel@gmail.com>
2012-10-08 15:23:27 +0200

    Fix comment.

--- testharness.js
+++ testharness.js
@@ -1872,7 +1872,7 @@ policies and contribution forms [3].
     }
 
     function forEach_windows(callback) {
-        // Iterate of the the windows [self ... top]. The callback is passed
+        // Iterate of the the windows [self ... top, opener]. The callback is passed
         // two objects, the first one is the windows object itself, the second one
         // is a boolean indicating whether or not its on the same origin as the
         // current window.
